6359bb965d41f69a2e27b502615f1022cfb64370
Parin <parin.patel@aunalytics.com>
2017-11-09 09:48:33 -0500

    fix

---
+++ setup_jenkins_slave_vm.sh
@@ -0,0 +1,18 @@
+vi /etc/sysconfig/network-scripts/ifcf0ens3
+
+
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+echo '{"hosts": ["tcp://0.0.0.0:2375"," unix:///var/run/docker.sock"]}' > /etc/docker/daemon.json
+systemctl restart docker && systemctl enable docker
+iptables -F
+yum install -y epel-release && yum groupinstall -y 'Development Tools'
+yum install -y net-tools bridge-utils
+pkill docker
+iptables -t nat -F
+ifconfig docker0 down
+brctl delbr docker0
+systemctl restart docker
+iptables -F
