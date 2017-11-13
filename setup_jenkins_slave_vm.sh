75178620e03488dd4d2f20b7d11762333b0aa5a1
Parin Patel <parin.patel@aunalytics.com>
2017-11-13 18:33:14 +0000

    file system to ovarlay2

--- setup_jenkins_slave_vm.sh
+++ setup_jenkins_slave_vm.sh
@@ -5,7 +5,7 @@ vi /etc/sysconfig/network-scripts/ifcf0ens3
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce
-echo '{"hosts": ["tcp://0.0.0.0:2375"," unix:///var/run/docker.sock"]}' > /etc/docker/daemon.json
+echo '{"hosts": ["tcp://0.0.0.0:2375"," unix:///var/run/docker.sock"],  "storage-driver": "overlay2","storage-opts": ["overlay2.override_kernel_check=true"]}' > /etc/docker/daemon.json
 systemctl restart docker && systemctl enable docker
 iptables -F
 yum install -y epel-release && yum groupinstall -y 'Development Tools'
